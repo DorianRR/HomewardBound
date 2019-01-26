a29f49b6ae0500310cec28876c119072a18f5c41
BosonHBC <a923000213@163.com>
2019-01-26 13:41:12 -0700

    Defalut movement

---
+++ Assets/Prototyping/Boson/Movement/CharacterMovement.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public enum MovementMode
+    {
+        Default,
+        Sliding,
+        Snagged
+    }
+
+    [Header("Force")]
+    [SerializeField]
+    private float fJumpForce;
+    [SerializeField]
+    private float fMovementForce;
+    [Header("Restrict")]
+    [SerializeField]
+    private float fRestrictAngle;
+    [SerializeField]
+    private float fMaxSpeed;
+    [SerializeField]
+    private float fJoyStickSensitivity;
+    public MovementMode moveMode = MovementMode.Default;
+    [SerializeField]
+    private float fRaycastDistance = 0.01f;
+
+    // Parameter
+    [SerializeField]
+    private bool bIsGrounded;
+    [SerializeField]
+    private int iControlID;
+
+    private string horiAxis = "Horizontal";
+    private string vertAxis = "Vertical";
+    private string rotXaxis = "Mouse X";
+    private string rotYaxis = "Mouse Y";
+
+    private string aButton = "Jump";
+
+    // Component
+    private Rigidbody rb;
+    private CapsuleCollider capCollider;
+    private Transform camParent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        capCollider = GetComponent<CapsuleCollider>();
+        camParent = transform.GetChild(0);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        bIsGrounded = IsGrounded();
+        PerformRotation();
+        PerformMovement();
+        PerformJumping();
+    }
+
+    public void SetControllerID(int _ID)
+    {
+        iControlID = _ID;
+
+        // Movement
+        horiAxis = "J" + iControlID + "Horizontal";
+        vertAxis = "J" + iControlID + "Vertical";
+
+        // Rotation
+        rotXaxis = "J" + iControlID + "Mouse X";
+        rotYaxis = "J" + iControlID + "Mouse Y";
+
+        // Jump
+        aButton = "J" + iControlID + "aButton";
+    }
+
+    private void PerformJumping()
+    {     
+   
+        if (Input.GetButtonDown(aButton) && bIsGrounded)
+        {
+            //Debug.Log("Press a button");
+            rb.AddForce(new Vector3(0, fJumpForce, 0));
+        }
+    }
+
+    private void PerformMovement()
+    {
+
+        float hori = Input.GetAxis(horiAxis);
+        float vert = Input.GetAxis(vertAxis);
+
+        Vector3 veloRef = rb.velocity;
+        veloRef.y = 0;
+        if (veloRef.magnitude < fMaxSpeed)
+            rb.AddRelativeForce(new Vector3(hori, 0, vert) * (bIsGrounded?fMovementForce:fMovementForce/2), ForceMode.Force);
+
+    }
+    private void PerformRotation()
+    {
+        float hori = Input.GetAxis(rotXaxis);
+        float vert = Input.GetAxis(rotYaxis);
+
+        //if (hori < 0.1f || hori > -0.1f)
+        //    hori = 0;
+        //if (vert < 0.1f || vert > -0.1f)
+        //    vert = 0;
+
+        if (iControlID == 0)
+            fJoyStickSensitivity = 1;
+
+        transform.Rotate(transform.up, hori * fJoyStickSensitivity);
+        camParent.Rotate(new Vector3(-vert * fJoyStickSensitivity, 0, 0));
+
+        if (camParent.localEulerAngles.x > fRestrictAngle && camParent.localEulerAngles.x < 180)
+            camParent.localEulerAngles = new Vector3(fRestrictAngle, 0, 0);
+        if (camParent.localEulerAngles.x < 360 - fRestrictAngle && camParent.localEulerAngles.x > 180)
+            camParent.localEulerAngles = new Vector3(360 - fRestrictAngle, 0, 0);
+    }
+    private bool IsGrounded()
+    {
+        bool _groudned = false;
+
+        Ray ray = new Ray(transform.position, Vector3.down);
+
+        Debug.DrawRay(ray.origin, ray.direction);
+
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit, fRaycastDistance))
+        {
+            if(!hit.collider.isTrigger)
+                _groudned = true;
+        }
+        
+
+        return _groudned;
+    }
+
+
+}
---
+++ Assets/Prototyping/Boson/Movement/ControllerMapping.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerMapping : MonoBehaviour
+{
+    public static ControllerMapping instance;
+    void Awake()
+    {
+        if (instance != this || instance == null)
+            instance = this;
+    }
+    List<CharacterMovement> lists = new List<CharacterMovement>();
+    [SerializeField] GameObject playerPrefab;
+    public int MaxSupportPlayer = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        JoystickAvaliable();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void JoystickAvaliable()
+    {
+        //Get Joystick Names
+        string[] temp = Input.GetJoystickNames();
+
+        //Check whether array contains anything
+        if (temp.Length > 0)
+        {
+            //Iterate over every element
+            for (int i = 0; i < temp.Length; ++i)
+            {
+                //Check if the string is empty or not
+                if (!string.IsNullOrEmpty(temp[i]))
+                {
+                    //Not empty, controller temp[i] is connected
+                    Debug.Log("Controller " + i + " is connected using: " + temp[i]);
+                    if (i < MaxSupportPlayer)
+                        CreatePlayer(i+1);
+                    else
+                        Debug.Log("Over support players, not going to spawn");
+                }
+                else
+                {
+                    //If it is empty, controller i is disconnected
+                    //where i indicates the controller number
+                    Debug.Log("Controller: " + i + " is disconnected.");
+
+                }
+            }
+        }
+    }
+
+    public void CreatePlayer(int _ID)
+    {
+        GameObject go = Instantiate(playerPrefab);
+        go.GetComponent<CharacterMovement>().SetControllerID(_ID);
+        go.name = "Player_" + _ID;
+        lists.Add(go.GetComponent<CharacterMovement>());
+    }
+}
