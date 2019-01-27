6ae054c63dc60b2e44a901a39e66253a046bc737
Breann Evans <breannkay44@gmail.com>
2019-01-27 10:56:51 -0700

    more stuff
    i think this is the rest of the assets that were made yesterday

---
+++ Assets/Prototyping/Breann/breakObjectParticleEffect.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class breakObjectParticleEffect : MonoBehaviour
+{
+    public ParticleSystem breakParticleSystem;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("floor"))
+        {
+            breakParticleSystem.EmitParams.transform = this.transform;
+            breakParticleSystem.Play();
+        }
+
+    }
+}
